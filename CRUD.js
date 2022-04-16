036231bb706c67875a623cf2626c5e2a2dd9b4fa
Your Name <you@example.com>
2022-04-16 13:54:49 -0500

    added javascript

---
+++ CRUD.js
@@ -0,0 +1,6 @@
+//add all code that does what we want it to do
+//create class for rooms and houses
+//create class for house service that enables us to send http request to preexisting api
+//class for DOM (clear)
+
+
